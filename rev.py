6d2c0fb007c3616c7cc56b2bc5eddd7a599a36a6
sush837 <sushmitareddy2286@gmail.com>
2025-09-01 16:05:42 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,6 +1,6 @@
-def str1(a):
-  str2 = ""
-  for b in a:
-    str2 = b + str2
+def str1(b):
+    str2 = ""
+    for a in b:
+        str2 = a+str2
     return str2
-print(str1("Hello"))
+print(str1('Hello'))
